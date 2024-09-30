39891cc2a80c8e0d66fccb3e9d2171e8dca50d07
himanshu <himanshunageshwar444@gmail.com>
2024-09-30 23:43:55 +0530

    configure webhooks

--- middleware.ts
+++ middleware.ts
@@ -8,11 +8,21 @@ export default authMiddleware({
     'api/uploadthing'
   ],
   ignoredRoutes: [
-    '/api/webhook/clerk',
+    // '/api/webhook/clerk',
+    '/api/webhooks(.*)',
     '/api/uploadthing'
   ]
 });
  
+// export const config = {
+//       matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
+
 export const config = {
-      matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
-};
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
