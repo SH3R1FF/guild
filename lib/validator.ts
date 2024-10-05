6bf0727abe483d7a81359975823000221b65243e
himanshu <himanshunageshwar444@gmail.com>
2024-10-06 02:18:47 +0530

    fix validator.ts

--- lib/validator.ts
+++ lib/validator.ts
@@ -6,7 +6,7 @@ export const projectFormSchema = z.object({
   description: z.string().min(150, 'Description should contain atleast 150 characters').max(400, 'Description must be less than 400 characters'),
   imageUrl: z.string().min(1), 
   // url: z.string().url('Invalid URL format').min(1, 'Project URL is required'),
-  url: z.string().url('Invalid URL format').optional(),
+  url: z.string().url('Invalid URL format').min(1, 'Project URL is required'),
   codeUrl: z.string().url('Invalid URL format').min(1, 'GitHub URL is required'),
   email: z.string().email('Invalid email format').min(1, 'Email is required'),
 });
